2681a61411be53c2d3e005e732808f6221fee393
zeXtreme <zerozwy@gmail.com>
2017-10-01 20:25:51 +0800

    mod script

--- he-ddns/he-dns-update.sh
+++ he-ddns/he-dns-update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while true
 do
-  curl -$type https://$hostname:$password@dyn.dns.he.net/nic/update?hostname=$hostname >> ddns.log
+  curl -$type -s -o ddns.log https://$hostname:$password@dyn.dns.he.net/nic/update?hostname=$hostname
+  cat ddns.log
   sleep 1h
 done
