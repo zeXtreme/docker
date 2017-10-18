ccb1b19a74cc648700246b3e53c4db8afc33cdb8
Zeng Weiyu <zerozwy@gmail.com>
2017-10-18 15:23:52 +0800

    Update heddns.py

--- he-ddns/heddns.py
+++ he-ddns/heddns.py
@@ -6,7 +6,7 @@ hostname = os.environ.get('hostname')
 password = os.environ.get('password')
 myip = os.environ.get('myip')
 url = 'https://' + hostname + ':' + password + '@dyn.dns.he.net/nic/update?hostname=' + hostname + ('' if myip == None else ('&myip=' + myip))
-while True
+while True:
     print('GET:', url)
     r = requests.get(url)
     print(r.text)
