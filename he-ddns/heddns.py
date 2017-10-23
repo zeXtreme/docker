63dba3227f7e8bee07d18433d4097c97487c7eb7
Zeng Weiyu <zerozwy@gmail.com>
2017-10-23 09:55:42 +0800

    Add files via upload

--- he-ddns/heddns.py
+++ he-ddns/heddns.py
@@ -1,13 +1,15 @@
 import urllib
+import ssl
 import time
 import os
 
+not_verify = ssl._create_unverified_context()
 hostname = os.environ.get('hostname')
 password = os.environ.get('password')
 myip = os.environ.get('myip')
 url = 'https://' + hostname + ':' + password + '@dyn.dns.he.net/nic/update?hostname=' + hostname + ('' if myip == None else ('&myip=' + myip))
 print('GET:', url)
 while True:
-    r = urllib.urlopen(url)
+    r = urllib.urlopen(url, context=not_verify)
     print(r.read())
     time.sleep(5 * 60)
